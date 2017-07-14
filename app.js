3af859a0c215bbf0862976396dae6bbc6b4896fb
Cheryl Lee <leecherylanne@gmail.com>
2017-07-13 23:56:25 -1000

    After watching a bunch of videos and then deconstructing someones sample code on code pen I figured it out Mostly

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById('button'),
+	count = 0; //this give it the start value
+
+button.onclick = function () {
+	count += 1; /// count at an interval of 1 or whatever purple number
+	button.innerHTML= "Clicks So Far: " + count; //tells the program to look for this and then change to Clicks so Far and add current count.
+};	
+
+
+
